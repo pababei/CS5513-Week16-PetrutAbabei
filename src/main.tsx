28840a3706dacc87d666f6c5664824b57665b483
pababei <petrut.ababei@gmail.com>
2023-12-02 09:50:20 -0800

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { defineCustomElements } from "@ionic/pwa-elements/loader";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+defineCustomElements(window);
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
